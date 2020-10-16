80f26181dc259b08857685f9c133270d62430cd9
Filipe do Couto Castro <filipe.couto.10@gmail.com>
2020-10-16 19:16:35 -0300

    Pontos de injeção de dependência e @Autowired.
    - Primeiro ponto: Construtor.
    - Segundo ponto: Método set
    - Terceiro ponto: No próprio atributo.
    - Utilizar a anotação @Autowired para definir o ponto de injeção. Ela também ajudará o Spring a selecionar o ponto correto em caso de uma possível ambiguidade(Por exemplo, quando se define dois construtores, aquele marcado com @Autowired será o selecionado pelo Spring para a instanciação do Bean).

--- projetos/algafood-api/src/main/java/com/castrofilipe/algafood/di/service/AtivacaoClienteService.java
+++ projetos/algafood-api/src/main/java/com/castrofilipe/algafood/di/service/AtivacaoClienteService.java
@@ -1,5 +1,6 @@
 package com.castrofilipe.algafood.di.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.castrofilipe.algafood.di.notificacao.Notificador;
@@ -10,8 +11,14 @@ public class AtivacaoClienteService {
 	
 	private Notificador notificador;
 	
+	//Ponto de injeção 1: Construtor
+	@Autowired
 	public AtivacaoClienteService(Notificador notificador) {
 		this.notificador = notificador;
+	}
+	
+	public AtivacaoClienteService(String parametroQualquer) {
+		
 	}
 	
 	public void ativar(Cliente cliente) {
