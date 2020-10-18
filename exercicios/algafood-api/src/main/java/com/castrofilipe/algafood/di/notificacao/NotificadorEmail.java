a6c0eec852aa176b9dcaeb768e85b19bf8d510e7
Filipe do Couto Castro <filipe.couto.10@gmail.com>
2020-10-18 18:24:15 -0300

    Criando classe de configuração e propriedades com a anotação @ConfigurationProperties
    - A classe de configuração facilita no processo de refatoração.

--- exercicios/algafood-api/src/main/java/com/castrofilipe/algafood/di/notificacao/NotificadorEmail.java
+++ exercicios/algafood-api/src/main/java/com/castrofilipe/algafood/di/notificacao/NotificadorEmail.java
@@ -1,28 +1,27 @@
 package com.castrofilipe.algafood.di.notificacao;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import com.castrofilipe.algafood.modelo.Cliente;
+import com.castrofilipe.algafood.properties.NotificadorProperties;
 
 @Profile("prod")
 @TipoDoNotificador(value = NivelUrgencia.NORMAL)
 @Component
 public class NotificadorEmail implements Notificador {
-	
-	@Value(value = "${notificador.email.host-servidor}")
-	private String host;
-	
-	@Value("${notificador.email.porta-servidor}")
-	private Integer porta;
+
+	@Autowired
+	private NotificadorProperties notificadorProperties;
 
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
-		System.out.printf("NotificadorEmail: Notificando %s através do e-mail %s: %s\n", 
-				cliente.getNome(), cliente.getEmail(), mensagem);
-		
-		System.out.printf("\nInfo servidor- Host: %s porta: %s", host, porta);
+		System.out.printf("NotificadorEmail: Notificando %s através do e-mail %s: %s\n", cliente.getNome(),
+				cliente.getEmail(), mensagem);
+
+		System.out.printf("\nInfo servidor- Host: %s porta: %s", notificadorProperties.getHostServidor(),
+				notificadorProperties.getPortaServidor());
 	}
-	
+
 }
---
+++ exercicios/algafood-api/src/main/java/com/castrofilipe/algafood/properties/NotificadorProperties.java
@@ -0,0 +1,40 @@
+package com.castrofilipe.algafood.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "notificador.email")
+public class NotificadorProperties {
+	
+	/**
+	 * Host do servidor de email
+	 * 
+	 * @author Filipe Castro
+	 * */
+	private String hostServidor;
+
+	/**
+	 * Porta do servidor de email
+	 * 
+	 * @author Filipe Castro
+	 * */
+	private Integer portaServidor = 20;
+
+	public String getHostServidor() {
+		return hostServidor;
+	}
+
+	public void setHostServidor(String hostServidor) {
+		this.hostServidor = hostServidor;
+	}
+
+	public Integer getPortaServidor() {
+		return portaServidor;
+	}
+
+	public void setPortaServidor(Integer portaServidor) {
+		this.portaServidor = portaServidor;
+	}
+	
+}
