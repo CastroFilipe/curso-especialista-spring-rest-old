8aa34a18331ab3ee978f0f64ee0bc1c597190623
Filipe do Couto Castro <filipe.couto.10@gmail.com>
2020-10-17 21:23:31 -0300

    Métodos de callback chamados na inicialização e destruição de beans.

--- exercicios/algafood-api/src/main/java/com/castrofilipe/algafood/di/service/AtivacaoClienteService.java
+++ exercicios/algafood-api/src/main/java/com/castrofilipe/algafood/di/service/AtivacaoClienteService.java
@@ -1,5 +1,8 @@
 package com.castrofilipe.algafood.di.service;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -16,6 +19,17 @@ public class AtivacaoClienteService {
 	@Autowired
 	public AtivacaoClienteService(@TipoDoNotificador(value = NivelUrgencia.NORMAL) Notificador notificador) {
 		this.notificador = notificador;
+	}
+	
+	@PostConstruct
+	private void init() {
+		System.out.println("Método chamado logo após a fase de INICIALIZAÇÃO do bean "
+				+ "(Somente após chamar o construtor)");
+	}
+	
+	@PreDestroy
+	private void preDestroy() {
+		System.out.println("Método chamado ANTES de iniciar a fase de DESTRUIÇÃO do bean");		
 	}
 
 	public void ativar(Cliente cliente) {
