a22f7c88bceb3f4541a9ed4da0c006df66ac35d4
Filipe do Couto Castro <filipe.couto.10@gmail.com>
2020-10-18 13:33:57 -0300

    Criando propriedades customizadas no arquivo .properties
    - Criação de propriedades customizadas
    - Injeção do valor da propriedade através da anotação @Value

--- exercicios/algafood-api/src/main/java/com/castrofilipe/algafood/di/notificacao/NotificadorEmail.java
+++ exercicios/algafood-api/src/main/java/com/castrofilipe/algafood/di/notificacao/NotificadorEmail.java
@@ -1,5 +1,6 @@
 package com.castrofilipe.algafood.di.notificacao;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -9,11 +10,19 @@ import com.castrofilipe.algafood.modelo.Cliente;
 @TipoDoNotificador(value = NivelUrgencia.NORMAL)
 @Component
 public class NotificadorEmail implements Notificador {
+	
+	@Value(value = "${notificador.email.host-servidor}")
+	private String host;
+	
+	@Value("${notificador.email.porta-servidor}")
+	private Integer porta;
 
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
 		System.out.printf("NotificadorEmail: Notificando %s através do e-mail %s: %s\n", 
 				cliente.getNome(), cliente.getEmail(), mensagem);
+		
+		System.out.printf("\nInfo servidor- Host: %s porta: %s", host, porta);
 	}
 	
 }
