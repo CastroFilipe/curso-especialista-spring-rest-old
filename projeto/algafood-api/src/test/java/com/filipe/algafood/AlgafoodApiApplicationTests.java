9de5bb44335e2d6c0e2bc19b2e831ce41197b064
Filipe do Couto Castro <filipe.couto.10@gmail.com>
2020-12-20 15:15:05 -0300

    Commit inicial

---
+++ projeto/algafood-api/src/test/java/com/filipe/algafood/AlgafoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filipe.algafood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
